90968ca3d8c48cf6a27a3c00dbf6563cf7d49a28
Henssouu <massou.milandou@yahoo.fr>
2023-08-02 14:53:56 +0200

    display user ok

--- routes/users.js
+++ routes/users.js
@@ -40,7 +40,7 @@ router.post('/signin', (req,res) => {
 
   User.findOne({ email: req.body.email }).then(data => {
     if (data && bcrypt.compareSync(req.body.password, data.password)) { //bcrypt.compareSync sert à comparer le mdp crypté avec celui rentré en dur lors de la connexion
-      res.json({ result: true, token: data.token });
+      res.json({ result: true, user: data });
     } else {
       res.json({ result: false, error: 'User not found or wrong password' });
     }
